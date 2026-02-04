11931769e07f581d368d93b968aa9b3f0d4c398d
ganovelli <fabio.ganovelli@gmail.com>
2026-02-04 21:22:22 +0100

    version done in class

--- src/code_00_raytracer/main_rt.cpp
+++ src/code_00_raytracer/main_rt.cpp
@@ -15,27 +15,31 @@ Few applications load it.
 One is IrfanView: https://www.irfanview.com/
 */
 struct image {
-	image(int _w, int _h) :w(_w), h(_h) { data.resize(w * h * 3, 0); }
+	image(int _w, int _h) :w(_w), h(_h) { data.resize(w * h * 3, 0); } // initialize buffer (RGB per pixel)
 	unsigned int w, h;
 
 	std::vector<int>  data;
 
+	// Set a pixel value (values expected in 0..255)
 	template <class S>
 	void set_pixel(int i, int j, S  r, S  g, S  b) {
-		j = h - 1 - j;
+		j = h - 1 - j; // flip vertically for image coordinate system
 		data[(j * w + i) * 3] = (unsigned char)r;
 		data[(j * w + i) * 3 + 1] = (unsigned char)g;
 		data[(j * w + i) * 3 + 2] = (unsigned char)b;
 	}
 
+	// Save image as ASCII PPM (P3) file
 	void save(const char* filename) {
 		ofstream f;
 		f.open(filename);
 		f << "P3\n";
 		f << w << " " << h << std::endl;
 
+		// max color value (use maximum found in buffer)
 		f << *(std::max_element(data.begin(), data.end())) << std::endl;
 
+		// write each pixel as "R G B" per line
 		for (unsigned int i = 0; i < data.size() / 3; ++i)
 			f << data[i * 3] << " " << data[i * 3 + 1] << " " << data[i * 3 + 2] << std::endl;
 		f.close();
@@ -44,70 +48,122 @@ struct image {
 
 
 struct p3 {
-	p3(){}
-	p3(float _x, float _y, float _z):x(_x), y(_y), z(_z){}
+	p3():x(0.f), y(0.f), z(0.f) {} // default constructor
+	p3(float _x, float _y, float _z):x(_x), y(_y), z(_z){} // value constructor
 
-	p3 operator -(p3 o) { return p3(o.x - x, o.y - y, o.z - z); }
-	float operator *(p3 o) { return x * o.x + y * o.y + z * o.z; }
+	// vector arithmetic helpers
+	p3 operator +(p3 o) { return p3(o.x + x, o.y + y, o.z + z); } // sum
+	p3 operator -(p3 o) { return p3(x -o.x , y  - o.y , z - o.z ); } //subtraction
+	float operator *(p3 o) { return x * o.x + y * o.y + z * o.z; } // dot product
+	p3 operator *(float s) { return p3(x*s, y * s,z* s); } // scalar multiply
 	float x, y, z;
 };
 
 struct ray {
-	ray(p3 o, p3 d):orig(o),dir(d){}
+	ray(p3 o, p3 d):orig(o),dir(d){} // origin and direction
 
 	p3 orig, dir;
 };
 
 struct sphere {
-	sphere(p3 c, float r, p3 col):center(c),radius(r),color(col){}
+	sphere(p3 c, float r, p3 col):center(c),radius(r),color(col){} // center, radius, color
 
 	p3 center, color;
 	float radius;
 };
 
-float  hit_sphere(ray r, sphere s) {
+struct hit_info {
+
+	hit_info() :hit(false), t(FAR_AWAY) {} // default: no hit, t very large
+	float t;   // distance along ray
+	p3 p;      // hit position
+	p3 n;      // surface normal at hit
+	bool hit;  // whether intersection occured
+	p3 color;  // object color
+};
+
+// Ray-sphere intersection: returns hit_info with nearest positive t if any
+hit_info  hit_sphere(ray r, sphere s) {
+	// coefficients for quadratic equation A t^2 + B t + C = 0
 	float A = r.dir* r.dir;
 	float B = r.dir*(r.orig - s.center) * 2;
 	float C = (r.orig - s.center)*(r.orig - s.center) - s.radius * s.radius;
 
 	float delta = B * B - 4 * A * C;
 
+	// no real roots -> no intersection
 	if (delta < 0)
-		return FAR_AWAY;
+		return hit_info();
 
-	float t = (-B - sqrt(B * B - 4 * A * C)) / (2 * A);
+	hit_info hi;
+	// choose smaller root first (closest intersection)
+	float t = (-B - sqrt(delta)) / (2 * A);
+	if( t <= 0 )
+		t = (-B + sqrt(delta)) / (2 * A);
+	// if still non-positive, intersection is behind ray origin
+	if (t <= 0)
+		return hit_info();
 
-	return t;
+	// fill hit information
+	hi.t = t;
+	hi.p =  r.orig + r.dir * t;
+	hi.n = hi.p - s.center;					 // unnormalized normal
+	hi.n = hi.n * (1.0 / sqrt(hi.n * hi.n)); // normalize normal
+	hi.color = s.color;
+	hi.hit = true;
+	return hi;
 }
 
 int main(int args, char** argv) {
 	int sx = 800;
 	int sy = 800;
-	image a(sx, sy);
+	image a(sx, sy); 
 
-	p3 eye = p3(0, 0, 0);
+	p3 eye = p3(0, 0, 0); // camera position
 
+	// scene setup: two spheres with colors
 	std::vector< sphere > scene;
 	scene.push_back(sphere(p3(0, 0, -3),   1.0, p3(255,0,0) ) );
-	scene.push_back(sphere(p3(0, 0, -4.0), 0.2, p3(0, 0, 255)));
+	scene.push_back(sphere(p3(0.6, 0.6, -2.0), 0.2, p3(0, 0, 255)));
 
+	p3 Lp = p3(1, 1, -1); // point light position
+
+	// iterate over image pixels (simple pinhole camera)
 	for (int i = 0; i < a.w; ++i)
 		for (int j = 0; j < a.h; ++j) {
-			p3 pixpos(-1 + 2 * i / float(a.w), -1 + 2 * j / float(a.h), -1);
-			ray r = ray(eye, pixpos - eye);
+			// compute pixel position on image plane in [-1,1] range
+			p3 pixpos(-1 + 2 * (i+0.5) / float(a.w), -1 + 2 * (j+0.5) / float(a.h), -1);
+			ray r = ray(eye, pixpos - eye); // primary ray
 
-			float min_t = FAR_AWAY;
-			p3 col = p3(0, 0, 0);
+			hit_info best_hi = hit_info(); // best intersection so far
+			p3 col = p3(0, 0, 0); // background color (black)
 			for (int is = 0; is < scene.size(); ++is) {
-				float t = hit_sphere(r, scene[is]);
-				if (t < min_t) {
-					min_t = t;
-					col = scene[is].color;
+				hit_info hi = hit_sphere(r, scene[is]); // test intersection
+				if (hi.t < best_hi.t) { // closer hit found
+					best_hi = hi;
+					p3 p = r.orig + r.dir * hi.t; // hit point
+					p3 L = Lp - p; // vector to light
+					L = L * (1.0 / sqrt(L * L)); // normalize L
+
+					// offset origin slightly to avoid self-intersection (shadow acne)
+					ray shadow_ray = ray(p + L*0.001, L);
+
+					// check for occlusion: if any object blocks the light, point is in shadow
+					int iss = 0;
+					for (; iss < scene.size(); ++iss)
+						if (hit_sphere(shadow_ray, scene[iss]).hit)
+							break;
+					
+					if (iss == scene.size()) { // light visible -> simple Lambertian shading
+						float cosLN = hi.n * L;
+						float al = max(0.f, cosLN); // clamp negative values
+						col = hi.color * al; // scale object color by diffuse term
+					}
 				}
 			}
-			a.set_pixel(i, j, col.x, col.y, col.z);
+			a.set_pixel(i, j, col.x, col.y, col.z); // write pixel
 		}
 
-	a.save("rendering.ppm");
+	a.save("rendering.ppm"); // save to disk
 	return 0;
 }
